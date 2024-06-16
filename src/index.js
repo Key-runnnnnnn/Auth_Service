fadf1c41210a17b16a51d2b6738c41303b8b09e1
kiran <kiran.s.dhawan@gmail.com>
2024-06-16 07:29:00 +0530

    Added get user by id in repository layer

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ const app = express();
 const {PORT} = require('./config/serverConfig'); 
 const apiRoutes = require('./routes/index');
 
-const prepareAndStartServer=()=>{
+// const UserRepository = require('./repository/user-repository')
+
+const prepareAndStartServer= async ()=>{
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use('/api', apiRoutes);
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    // const userRepository = new UserRepository();
+    // const user =  await userRepository.getById(1);
+    // console.log(user);
+
     console.log(`Server is running on port ${PORT}`);
   });
 }
