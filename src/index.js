21dd6e25bbac6b8325bb344882f3aa27174eea03
kiran <kiran.s.dhawan@gmail.com>
2024-06-17 12:57:25 +0530

    Add JWT create and verify logic

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const {PORT} = require('./config/serverConfig'); 
+const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const UserRepository = require('./repository/user-repository')
+// const UserService = require('./services/user-service')
 
-const prepareAndStartServer= async ()=>{
+const prepareAndStartServer = async () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use('/api', apiRoutes);
   app.listen(PORT, async () => {
-    // const userRepository = new UserRepository();
-    // const user =  await userRepository.getById(4);
-    // console.log(user);
-
     console.log(`Server is running on port ${PORT}`);
+    // const userService = new UserService();
+    // const newToken = userService.createToken({ email: 'kiran@admin234.com', id: 1 });
+    // console.log("new token is", newToken);
+
+    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImVtYWlsIjoia2lyYW5AYWRtaW4yMzQuY29tIiwiaWQiOjF9LCJpYXQiOjE3MTg2MDg3MzIsImV4cCI6MTcxODYxMjMzMn0.fR5FVyzCj7asLaGnaeAlevuiN3MtWFaW5dALOBKBitQ"
+
+    // const verify= userService.verifyToken(token)
+    // console.log("verify", verify)
   });
 }
 
