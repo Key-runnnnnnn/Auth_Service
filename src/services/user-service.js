8dea883632ef79731a7a2ca5324f4474b6457a83
kiran <kiran.s.dhawan@gmail.com>
2024-06-17 13:04:24 +0530

    Added function to compare the incomming password with the encrypted password in the database.

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,13 +1,14 @@
 const UserRepository = require('../repository/user-repository')
 const jwt = require('jsonwebtoken');
+const { JWT_KEY } = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 
-const {JWT_KEY} = require('../config/serverConfig');
-class UserService{
-  constructor(){
+class UserService {
+  constructor() {
     this.userRepository = new UserRepository();
   }
 
-  async create(data){
+  async create(data) {
     try {
       const user = await this.userRepository.create(data);
       return user;
@@ -17,7 +18,7 @@ class UserService{
     }
   }
 
-  async destroy(userId){
+  async destroy(userId) {
     try {
       await this.userRepository.destroy(userId);
     } catch (error) {
@@ -26,25 +27,35 @@ class UserService{
     }
   }
 
-  createToken(user){
+  createToken(user) {
     try {
-      const token = jwt.sign({user}, JWT_KEY, {expiresIn: '1h'});
-      return token; 
+      const token = jwt.sign({ user }, JWT_KEY, { expiresIn: '1h' });
+      return token;
     } catch (error) {
       console.log("Something went wrong on generating web token");
       throw error;
     }
   }
 
-  verifyToken(token){
+  verifyToken(token) {
     try {
       const decoded = jwt.verify(token, JWT_KEY);
       return decoded;
     } catch (error) {
-      console.log("Something went wrong on verifying web token",error);
+      console.log("Something went wrong on verifying web token", error);
       throw error;
     }
   }
+
+  checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+    } catch (error) {
+      console.log("Something went wrong in password comparison");
+      throw error;
+    }
+  }
+
 }
 
 module.exports = UserService;
