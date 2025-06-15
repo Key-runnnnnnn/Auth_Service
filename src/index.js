f1e65ab59f705a6105df09433387aa1f0b8b824b
kiran <kiran.s.dhawan@gmail.com>
2025-06-15 17:28:26 +0530

    Admin

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
 const db = require('./models/index');
-
+const {User, Role} = require('./models/index');
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -20,6 +20,15 @@ const prepareAndStartServer = () => {
         if(process.env.DB_SYNC) {
             db.sequelize.sync({alter: true});
         }
+
+        // const u1 = await User.findByPk(3);
+        // const r1 = await Role.findByPk(1);
+        // if (u1 && r1) {
+        //     await u1.addRole(r1);
+        //     console.log('Role added to user');
+        // } else {
+        //     console.log('User or Role not found');
+        // }
     });
 }   
 
